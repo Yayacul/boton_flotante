07a13db4e47a875a329836a205cf956b092443bc
LIZZ_YADARI_RIOS_GOMEZ <a.20308051281027@cbtis128.edu.mx>
2023-05-18 21:36:45 +0000

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/boton_flotante.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,35 +9,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+
+      //quitamos debug
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.blue, 
+        //configurar texto
+        textTheme: TextTheme(
+          bodyText2:TextStyle(
+            color: Colors.pink,
+            fontSize: 20
+          )
+        )
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      //pagina inicial
+      home: BotonFlotante(),
     );
   }
 }
---
+++ lib/screens/boton_flotante.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+//crea la clase y el estado a cambiar
+class BotonFlotante extends StatefulWidget {
+  const BotonFlotante({super.key});
+  _BotonFlotante createState() => _BotonFlotante();
+}
+
+//crear los widgets a mostrar
+class _BotonFlotante extends State<BotonFlotante> {
+  String txt="";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Boton Flotante'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(txt)
+          ],
+        )
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+           FloatingActionButton(
+                    child: Icon(Icons.visibility),
+                    onPressed: () => setState(() => txt="Hola mundo")
+                  ),
+
+        FloatingActionButton(
+                    child: Icon(Icons.visibility_off),
+                    onPressed: () => setState(() => txt="")
+                  ),
+        ]
+
+      )
+    );
+  }
+}
